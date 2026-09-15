a7efb0f260357a79be2767bdab2c9fd126ea34ae
Hustar0311 <ruanzy0311@gmail.com>
2026-09-15 16:13:09 +0800

    Fix Kotlin compiler target configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -35,10 +37,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     buildFeatures {
         compose = true
         buildConfig = false
@@ -49,6 +47,12 @@ android {
         "META-INF/LICENSE*",
         "META-INF/NOTICE*",
     )
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
 }
 
 dependencies {
